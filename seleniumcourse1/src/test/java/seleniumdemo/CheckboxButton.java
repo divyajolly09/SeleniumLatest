0930393a0f1d4245b5644817e7beba33d644a763
josep <josep@MSI>
2024-04-07 16:15:02 +0530

    adding code of checkbox nd radio button

--- seleniumcourse1/src/test/java/seleniumdemo/CheckboxButton.java
+++ seleniumcourse1/src/test/java/seleniumdemo/CheckboxButton.java
@@ -13,10 +13,65 @@ public class CheckboxButton extends Base {
 		Boolean isSingleCheckbox = singleCheckBox.isSelected();
 		System.out.println(isSingleCheckbox);
 	}
+	public void verifyMultiplecheckBoxDemo()
+	{
+		//Verify all four check boxes are unselected by default
+		
+		WebElement checkBoxOne=driver.findElement(By.xpath("//input[@id='check-box-one']"));
+		boolean isCheckBoxOneIsSelected=checkBoxOne.isSelected();
+		System.out.println(isCheckBoxOneIsSelected);
+		
+		WebElement checkBoxTwo=driver.findElement(By.xpath("//input[@id='check-box-two']"));
+		boolean isCheckBoxTwoIsSelected=checkBoxTwo.isSelected();
+		System.out.println(isCheckBoxTwoIsSelected);
+		
+		WebElement checkBoxThree=driver.findElement(By.xpath("//input[@id='check-box-three']"));
+		boolean isCheckBoxThreeIsSelected=checkBoxThree.isSelected();
+		System.out.println(isCheckBoxThreeIsSelected);
+		
+		WebElement checkBoxfour=driver.findElement(By.xpath("//input[@id='check-box-four']"));
+		boolean isCheckBoxFourIsSelected=checkBoxfour.isSelected();
+		System.out.println(isCheckBoxFourIsSelected);
+		
+		//Verify all four checkboxes are checked when SelectAll button is clicked
+		WebElement selectAllButton=driver.findElement(By.xpath("//input[@id='button-two']"));
+		selectAllButton.click();
+		
+		isCheckBoxOneIsSelected=checkBoxOne.isSelected();
+		System.out.println(isCheckBoxOneIsSelected);
+		isCheckBoxTwoIsSelected=checkBoxTwo.isSelected();
+		System.out.println(isCheckBoxTwoIsSelected);
+		isCheckBoxThreeIsSelected=checkBoxThree.isSelected();
+		System.out.println(isCheckBoxThreeIsSelected);
+		isCheckBoxFourIsSelected=checkBoxfour.isSelected();
+		System.out.println(isCheckBoxFourIsSelected);
+		
+		//Verify Button name is changed to UnselectAll
+		String buttonName = selectAllButton.getAttribute("value");
+		System.out.println(buttonName);
+		
+		//Verify all four checkboxes are unchecked when UnselectAll button is clicked
+		selectAllButton.click();
+		
+		isCheckBoxOneIsSelected=checkBoxOne.isSelected();
+		System.out.println(isCheckBoxOneIsSelected);
+		isCheckBoxTwoIsSelected=checkBoxTwo.isSelected();
+		System.out.println(isCheckBoxTwoIsSelected);
+		isCheckBoxThreeIsSelected=checkBoxThree.isSelected();
+		System.out.println(isCheckBoxThreeIsSelected);
+		isCheckBoxFourIsSelected=checkBoxfour.isSelected();
+		System.out.println(isCheckBoxFourIsSelected);
+		
+		//Verify Button name is changed to SelectAll
+		String buttonName1 = selectAllButton.getAttribute("value");
+		System.out.println(buttonName1);		
+	}
+	
 	public static void main(String[] args) {
 		CheckboxButton checkboxbutton = new CheckboxButton();
 		checkboxbutton.initializeBrowser();
 		checkboxbutton.verifySingleCheckBoxDemo();
+		checkboxbutton.verifyMultiplecheckBoxDemo();
 		checkboxbutton.browserQuit();
 		// TODO Auto-generated method stub
 
--- seleniumcourse1/src/test/java/seleniumdemo/RadioButton.java
+++ seleniumcourse1/src/test/java/seleniumdemo/RadioButton.java
@@ -4,13 +4,36 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class RadioButton extends Base {
-	public void radioButtonDemo()
+	public void verifySingleRadioButtonIsUnselected()
 	{
 		driver.navigate().to("https://selenium.qabible.in/radio-button-demo.php");
+		//Verify radio buttons are unchecked by default
 		WebElement maleRadioButton=driver.findElement(By.xpath("//input[@id='inlineRadio1']"));
-		maleRadioButton.click();
 		boolean isMaleRadioButtonSelected=maleRadioButton.isSelected();
 		System.out.println(isMaleRadioButtonSelected);
+		WebElement FemaleRadioButton=driver.findElement(By.xpath("//input[@id='inlineRadio2']"));
+		boolean isFemaleRadioButtonSelected=FemaleRadioButton.isSelected();
+		System.out.println(isFemaleRadioButtonSelected);
+		
+		//Verify male radio button is clicked and selected
+		maleRadioButton.click();
+		isMaleRadioButtonSelected=maleRadioButton.isSelected();
+		System.out.println(isMaleRadioButtonSelected);
+	}
+	
+	public void groupButtonDemo()
+	{
+		//Verify Male Radio button in multiple group is selected
+		WebElement maleRadioButton1=driver.findElement(By.xpath("//input[@id='inlineRadio11']"));
+		maleRadioButton1.click();
+		boolean isMaleradioButtonNotSelected=maleRadioButton1.isSelected();
+		System.out.println(isMaleradioButtonNotSelected);
+		
+		//Verify Patient Age Radio button is selected
+		WebElement patientAgeRadiobutton=driver.findElement(By.xpath("//input[@id='inlineRadio22']"));
+		patientAgeRadiobutton.click();
+		boolean isPatientRadiooButtonselected=patientAgeRadiobutton.isSelected();
+		System.out.println(isPatientRadiooButtonselected);
 	}
 
 	public static void main(String[] args) {
@@ -18,10 +41,9 @@ public class RadioButton extends Base {
 		// TODO Auto-generated method stub
 		RadioButton radiobutton=new RadioButton();
 		radiobutton.initializeBrowser();
-		radiobutton.radioButtonDemo();
+		radiobutton.verifySingleRadioButtonIsUnselected();
+		radiobutton.groupButtonDemo();
 		radiobutton.browserQuit();
-		
-
 	}
 
 }
